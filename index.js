1f7688648932c45b00ba4bf5e10ebc2552f4b80b
“nsandobal” <nikosanarg@gmail.com>
2021-05-19 13:38:48 -0300

    Visual improve for Resumen

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ function change_practicas_status() {
 
 function show_resume() {
     let resumen = document.getElementById("resume-p");
+    let r_button = document.getElementById("resume-button");
     if (resumen_open) {
         resumen.classList.remove('resume-expand');
         resumen.classList.add('resume-contract');
+        r_button.style.transform = "rotate(0deg)";
     } else {
         resumen.classList.remove('resume-contract');
         resumen.classList.add('resume-expand');
-    }
+        r_button.style.transform = "rotate(180deg)";
+    }    
     resumen_open ^= true;
 }
-"fas fa-sort-down fa-2x"
 
 /*======================= VUE.js =========================*/
 
@@ -28,7 +30,6 @@ var app = new Vue({
     el:'#app',
     data:{
         page_title: 'Astronomía para gente apurada',
-        message: 'ASTRONOMIA PARA GENTE APURADA'
     },
 });
 
