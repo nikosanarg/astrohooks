911feade72bd3317f8b4a8ecde3b0750e167b84d
“nsandobal” <nikosanarg@gmail.com>
2021-05-21 01:23:36 -0300

    req Practica 1 conversion de unidades

--- js/calc.js
+++ js/calc.js
@@ -31,19 +31,65 @@ function px_clean(prefix) {
     }
 }
 
+const p1_select_options = ['p1-x1', 'p1-x2', 'p1-x3', 'p1-x4'];
+function p1_change_type() {
+    let opt = document.getElementById("p1-select").value;
+    p1_select_options.forEach(i => {
+        if (i == opt) {
+            document.getElementById(i).classList.add('visible');
+            document.getElementById(i).classList.remove('invisible');
+        } else {
+            document.getElementById(i).classList.add('invisible');
+            document.getElementById(i).classList.remove('visible');
+        } 
+    });
+}
+
 function p1_x1_calc() {
     let i1 = document.getElementById('p1-x1-i1').value - 0;
-    let i2 = document.getElementById('p1-x1-i2').value - 0;
-    let i3 = document.getElementById('p1-x1-i3').value - 0;
-    document.getElementById('p1-x1-o1').value = parseFloat((i1 + (i2 / 60) + (i3 / 3600)).toFixed(5)) + "°";
+    let grades = Math.trunc(i1);
+    let minutes = Math.trunc((i1 - grades) * 60 + 0.0001);
+    let seconds = Math.trunc((((i1 - grades) * 60) - minutes) * 60 + 0.0001);
+    document.getElementById('p1-x1-o1').value = parseFloat(grades) + "° " + parseFloat(minutes) + "' " + parseFloat(seconds) + "''";
 }
 
 function p1_x2_calc() {
     let i1 = document.getElementById('p1-x2-i1').value - 0;
-    let grades = Math.trunc(i1);
-    document.getElementById('p1-x2-o1').value = parseFloat(grades) + "°";
-    let minutes = Math.trunc((i1 - grades) * 60 + 0.00001);
-    document.getElementById('p1-x2-o2').value = parseFloat(minutes) + "'";
-    let seconds = Math.trunc((((i1 - grades) * 60) - minutes) * 60 + 0.00001);
-    document.getElementById('p1-x2-o3').value = parseFloat(seconds) + '"';
+    let i2 = document.getElementById('p1-x2-i2').value - 0;
+    let i3 = document.getElementById('p1-x2-i3').value - 0;
+    let grados = parseFloat((i1 + (i2 / 60) + (i3 / 3600)).toFixed(5));
+    document.getElementById('p1-x2-o1').value = grados + "°";
+    document.getElementById('p1-x2-o2').value = parseFloat((grados * 3.1415926535 / 180).toFixed(5)) + " rad";
+    let hs = Math.trunc(grados * 12 / 180); 
+    let min = Math.trunc(((grados * 12 / 180) - hs) * 60 + 0.0001); 
+    let seg = Math.trunc(((((grados * 12 / 180) - hs) * 60) - min) * 60 + 0.0001); 
+    document.getElementById('p1-x2-o3').value = hs + "hs " + min + "min " + seg + "seg";
+}
+
+function p1_x3_calc() {
+    let i1 = document.getElementById("p1-x3-i1").value - 0;
+    let grados = parseFloat((i1 * 180 / 3.1415926535).toFixed(5));
+    document.getElementById('p1-x3-o1').value = grados + "°";
+    let grades = Math.trunc(grados);
+    let minutes = Math.trunc((grados - grades) * 60 + 0.0001);
+    let seconds = Math.trunc((((grados - grades) * 60) - minutes) * 60 + 0.0001);
+    document.getElementById('p1-x3-o2').value = parseFloat(grades) + "° " + parseFloat(minutes) + "' " + parseFloat(seconds) + "''";
+    let hs = Math.trunc(grados * 12 / 180); 
+    let min = Math.trunc(((grados * 12 / 180) - hs) * 60 + 0.0001); 
+    let seg = Math.trunc(((((grados * 12 / 180) - hs) * 60) - min) * 60 + 0.0001); 
+    document.getElementById('p1-x3-o3').value = hs + "hs " + min + "min " + seg + "seg";
+}
+
+function p1_x4_calc() {
+    let i1 = document.getElementById('p1-x4-i1').value - 0;
+    let i2 = document.getElementById('p1-x4-i2').value - 0;
+    let i3 = document.getElementById('p1-x4-i3').value - 0;
+    let grados = parseFloat(((i1 + (i2 / 60) + (i3 / 3600)) * 180 / 12).toFixed(5));
+    document.getElementById('p1-x4-o1').value = grados + "°";
+    document.getElementById('p1-x4-o2').value = parseFloat((grados * 3.1415926535 / 180).toFixed(5)) + " rad";
+    let grades = Math.trunc(grados);
+    let minutes = Math.trunc((grados - grades) * 60 + 0.0001);
+    let seconds = Math.trunc((((grados - grades) * 60) - minutes) * 60 + 0.0001);
+    document.getElementById('p1-x4-o3').value = parseFloat(grades) + "° " + parseFloat(minutes) + "' " + parseFloat(seconds) + "''";
+
 }
